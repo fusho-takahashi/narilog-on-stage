c82a30a8366ab55c8fc5d0f2b2c7aaee3f877d53
fusho-takahashi <zenryaku@fusho-takahashi.me>
2021-02-26 08:23:53 +0900

    set favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../../styles/globals.css'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <Component {...pageProps} />
+  <>
+    <Head>
+      <link
+        rel="icon"
+        href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/271/cloud_2601-fe0f.png"
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>
 )
 
 export default App
